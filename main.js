c64183a0d24418bd6b28860292c8225ef8b39cc8
mukeke <mukeke@quandashi.com>
2019-01-11 16:03:06 +0800

    音乐播放器

---
+++ main.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+    // 创建浏览器窗口
+    win = new BrowserWindow({ width: 800, height: 600 })
+    win.webContents.openDevTools();
+    // 然后加载应用的 index.html。
+    win.loadFile('music/index.html')
+}
+
+app.on('ready', createWindow);
